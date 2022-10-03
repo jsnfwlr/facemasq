13cd5c8daf3ae84d3e23b9ddce81cb052b88fd2f
Jason Fowler <phalacee@gmail.com>
2022-10-03 14:06:12 +0800

    add i18n to web

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,6 +2,24 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import "./styles/index.scss"
 import router from "./routes"
+import { createI18n } from "vue-i18n"
 import { createPinia } from "pinia"
 
-createApp(App).use(router).use(createPinia()).mount("#app")
+import en from "../../i18n/web/facemasq-en.json"
+import fr from "../../i18n/web/facemasq-fr.json"
+
+type MessageSchema = typeof en | typeof fr
+
+const i18n = createI18n<[MessageSchema], "en" | "fr">({
+  locale: "en-US",
+  messages: {
+    en: en,
+    fr: fr,
+  },
+})
+
+const app = createApp(App)
+app.use(router)
+app.use(i18n)
+app.use(createPinia())
+app.mount("#app")
