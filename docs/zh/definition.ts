cb6cf752810573a0a1685119d83aa57d58d02dbf
Jason Fowler <phalacee@gmail.com>
2022-10-07 09:30:42 +0800

    Continuing on docs

--- docs/zh/definition.ts
+++ docs/zh/definition.ts
@@ -11,71 +11,15 @@ export default {
       "items": [
         {
           "text": "About",
-          "link": "/zh/"
+          "link": "/es/"
         },
         {
           "text": "License",
-          "link": "/zh/license"
+          "link": "/es/license"
         },
         {
           "text": "Change log",
-          "link": "/zh/changelog"
-        }
-      ]
-    },
-    {
-      "text": "安装指南",
-      "collapsible": true,
-      "items": [
-        {
-          "text": "介绍",
-          "link": "/zh/guide/introduction"
-        },
-        {
-          "text": "安装指南",
-          "link": "/zh/guide/installation"
-        },
-        {
-          "text": "入门",
-          "link": "/zh/guide/getting-started"
-        },
-        {
-          "text": "管理记录",
-          "link": "/zh/guide/managing-records"
-        },
-        {
-          "text": "高级安装指南",
-          "link": "/zh/guide/advanced-installation"
-        },
-        {
-          "test": "理解错误",
-          "link": "/zh/guide/errors/"
-        }
-      ]
-    },
-    {
-      "text": "Developer Docs",
-      "collapsible": true,
-      "items": [
-        {
-          "text": "Introduction",
-          "link": "/zh/dev/introduction"
-        },
-        {
-          "text": "Dependencies",
-          "link": "/zh/dev/dependencies"
-        },
-        {
-          "text": "Contributing",
-          "link": "/zh/dev/contributing"
-        },
-        {
-          "text": "API Documentation",
-          "link": "/zh/dev/api-documentation"
-        },
-        {
-          "text": "UI Documentation",
-          "link": "/zh/dev/ui-documentation"
+          "link": "/es/changelog"
         }
       ]
     }
